a1f783fcdbd1f993d7704ff01ad4cf503c7339a9
fatimagoncalves <124080552+fatimagoncalves@users.noreply.github.com>
2024-09-06 16:52:18 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace SQLi_1
                 var user = args[0];
                 var pwd = Encrypt(args[1]);
                 Login(user, pwd);
+                password = "1234";
             }
             catch  
             {
